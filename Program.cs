7edf3ace4a09795b1b5333588893dd09b6c67889
Lucas Brynk Jensen <lucasbrynk@hotmail.com>
2019-03-06 14:34:05 +0100

    yes

---
+++ Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace digitalClock
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ClockDisplay clock = new ClockDisplay(23, 59);
+            Console.WriteLine(clock.display);
+            clock.TimeTick();
+            Console.WriteLine(clock.display);
+            Console.ReadLine();
+            
+        }
+    }
+
+    class NumberDisplay
+    {
+        public int value;
+        int limit;
+        
+
+        public NumberDisplay(int rolloverLimit)
+        {
+            limit = rolloverLimit;
+            value = 0;
+        }
+
+        public void increment()
+        {
+            value = (value + 1) % limit;
+        }
+
+        public string getDisplayValue()
+        {
+            if (value < 10)
+            {
+                return "0" + value;
+            }
+            else
+            {
+                return "" + value;
+            }
+        }
+
+        public void setDisplayValue(int newValue)
+        {
+            if(newValue >= 0 && newValue < limit)
+                value = newValue;
+        }
+
+    }
+
+    class ClockDisplay
+    {
+        NumberDisplay hours;
+        NumberDisplay minutes;
+        public string display;
+
+        public ClockDisplay()
+        {
+            hours = new NumberDisplay(24);
+            minutes = new NumberDisplay(60);
+            UpdateDisplay();
+        }
+
+        public ClockDisplay(int hour, int minute)
+        {
+            hours = new NumberDisplay(24);
+            minutes = new NumberDisplay(60);
+            SetTime(hour, minute);
+            UpdateDisplay();
+        }
+
+
+        void SetTime(int hour, int minute)
+        {
+            hours.setDisplayValue(hour);
+            minutes.setDisplayValue(minute);
+        }
+
+        public void TimeTick()
+        {
+            minutes.increment();
+            if(minutes.value == 0)
+            {
+                hours.increment();
+            }
+            UpdateDisplay();
+        }
+
+        public void UpdateDisplay()
+        {
+
+            display = hours.getDisplayValue() + ":" + minutes.getDisplayValue();
+        }
+    }
+}
